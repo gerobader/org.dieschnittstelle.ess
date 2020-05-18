17dbc49032aa0382e2955013268a90ffe85b79f0
Gero Bader <gero.bader@web.de>
2020-05-18 21:21:55 +0200

    ser-3 task

--- org.dieschnittstelle.ess.ser.client/src/main/java/org/dieschnittstelle/ess/ser/client/ShowTouchpointService.java
+++ org.dieschnittstelle.ess.ser.client/src/main/java/org/dieschnittstelle/ess/ser/client/ShowTouchpointService.java
@@ -1,13 +1,17 @@
 package org.dieschnittstelle.ess.ser.client;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.concurrent.Future;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.Logger;
@@ -210,37 +214,53 @@ public class ShowTouchpointService {
 		try {
 
 			// create post request for the api/touchpoints uri
-
+			HttpPost request = new HttpPost("http://localhost:8888/org.dieschnittstelle.ess.ser/api/touchpoints");
 			// create an ObjectOutputStream from a ByteArrayOutputStream - the
 			// latter must be accessible via a variable
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
 
 			// write the object to the output stream
+			show("baos before: %s", baos);
+			oos.writeObject(tp);
+			show("baos after: %s", baos);
 
 			// create a ByteArrayEntity and pass it the byte array from the
 			// output stream
+			ByteArrayEntity bae = new ByteArrayEntity(baos.toByteArray());
 
 			// set the entity on the request
+			request.setEntity(bae);
 
 			// execute the request, which will return a Future<HttpResponse> object
+			Future<HttpResponse> responseFuture = client.execute(request, null);
 
 			// get the response from the Future object
+			HttpResponse response = responseFuture.get();
 
 			// log the status line
 
+
 			// evaluate the result using getStatusLine(), use constants in
 			// HttpStatus
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_CREATED) {
+				/* if successful: */
+				// create an object input stream using getContent() from the
+				// response entity (accessible via getEntity())
+				ObjectInputStream ois = new ObjectInputStream(response.getEntity().getContent());
 
-			/* if successful: */
+				// read the touchpoint object from the input stream
+				AbstractTouchpoint createdTouchpoint = (AbstractTouchpoint) ois.readObject();
+				show("created touchpoint: %s", createdTouchpoint);
 
-			// create an object input stream using getContent() from the
-			// response entity (accessible via getEntity())
+				// cleanup the request
+				EntityUtils.consume(response.getEntity());
 
-			// read the touchpoint object from the input stream
+				// return the object that you have read from the response
+				return createdTouchpoint;
+			}
 
-			// cleanup the request
-			// EntityUtils.consume(response.getEntity());
 
-			// return the object that you have read from the response
 			return null;
 		} catch (Exception e) {
 			logger.error("got exception: " + e, e);
--- org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointServiceServlet.java
+++ org.dieschnittstelle.ess.ser/src/main/java/org/dieschnittstelle/ess/ser/TouchpointServiceServlet.java
@@ -1,5 +1,7 @@
 package org.dieschnittstelle.ess.ser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import javax.servlet.http.HttpServlet;
@@ -8,7 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
+import org.apache.http.HttpResponse;
 import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 
 public class TouchpointServiceServlet extends HttpServlet {
 
@@ -48,39 +52,42 @@ public class TouchpointServiceServlet extends HttpServlet {
 		}
 
 	}
-	
-	/*
+
 	@Override	
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
 		// assume POST will only be used for touchpoint creation, i.e. there is
 		// no need to check the uri that has been used
 
 		// obtain the executor for reading out the touchpoints from the servlet context using the touchpointCRUD attribute
+		TouchpointCRUDExecutor exec = (TouchpointCRUDExecutor) getServletContext().getAttribute("touchpointCRUD");
 
 		try {
 			// create an ObjectInputStream from the request's input stream
+			ObjectInputStream ois = new ObjectInputStream(request.getInputStream());
 		
 			// read an AbstractTouchpoint object from the stream
+			AbstractTouchpoint tp = (AbstractTouchpoint) ois.readObject();
 		
 			// call the create method on the executor and take its return value
+			tp = exec.createTouchpoint(tp);
 		
 			// set the response status as successful, using the appropriate
 			// constant from HttpServletResponse
+			response.setStatus(HttpServletResponse.SC_CREATED);
 		
 			// then write the object to the response's output stream, using a
 			// wrapping ObjectOutputStream
+			ObjectOutputStream oos = new ObjectOutputStream(response.getOutputStream());
+
 		
 			// ... and write the object to the stream
-		
+			oos.writeObject(tp);
+
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 
 	}
-	*/
-
-
-	
 }
